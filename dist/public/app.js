29a355124828a41e32fbf4cf0256d1ef1dc9440c
Harteg Wariyar <greendost@users.noreply.github.com>
2019-07-20 14:01:03 -0400

    initial commit: this is a react cdn + node setup; we can change it later to include a wepback / babel setup

---
+++ dist/public/app.js
@@ -0,0 +1,7 @@
+const App = () => (
+  <main>
+    <h1>hello from react</h1>
+  </main>
+);
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+var app = express();
+const PORT = process.env.PORT || 3001;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// middleware
+app.use((req, res, next) => {
+  console.log(`req.url=${req.url}`);
+  next();
+});
+app.use(express.static('./dist/public'));
+app.use(bodyParser.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT}`);
+});
